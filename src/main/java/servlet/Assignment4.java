910a9652c19ec86ebefcef0e4dde9d10014bb43f
charlielduong <charlielduong@gmail.com>
2022-03-08 04:46:10 -0500

    Final Changes to Assignment 4

--- src/main/java/servlet/Assignment4.java
+++ src/main/java/servlet/Assignment4.java
@@ -11,140 +11,138 @@ import javax.servlet.annotation.WebServlet;
 
 public class Assignment4 extends HttpServlet // Inheriting from HttpServlet makes this a servlet
 {
-  public void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    response.setContentType("text/html");
-    PrintWriter out = response.getWriter();
-    printHeader(out);
-    String[] characteristics = request.getParameterValues("blocks");
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-    out.println("<body>");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        printHeader(out);
+        String[] characteristics = request.getParameterValues("blocks");
 
-    out.println("<div>");
-    out.println("  <div style='text-align: center;'>");
-    out.println("    <h1 style='margin-bottom: 0px; padding-top: 5%;'>SWE 432-001 ASSIGNMENT #4</h1>");
-    out.println("    <h2>Charlie Duong &nbsp; | &nbsp; Khai Nguyen &nbsp; | &nbsp; Jesse Tran</h2><br>");
-    out.println("    <h3>Characteristics and their blocks</h3>");
-    out.println("  </div>");
+        out.println("<body>");
 
-    out.println("  <div style='background-color: white; border-radius: 5px; width: 50%; margin: auto; padding: 1em;'>");
+        out.println("<div>");
+        out.println("  <div style='text-align: center;'>");
+        out.println("    <h1 style='margin-bottom: 0px; padding-top: 5%;'>SWE 432-001 ASSIGNMENT #4</h1>");
+        out.println("    <h2>Charlie Duong &nbsp; | &nbsp; Khai Nguyen &nbsp; | &nbsp; Jesse Tran</h2><br>");
+        out.println("    <h3>Characteristics and their blocks</h3>");
+        out.println("  </div>");
 
-    int name = 1;
-    for (String block : characteristics) {
-      String line = "";
-      line += "<p> Characteristic " + name + ": [ ";
-      int numBlocks = Integer.parseInt(block);
+        out.println("  <div style='background-color: white; border-radius: 5px; width: 50%; margin: auto; padding: 1em;'>");
 
-      for (int i = 1; i <= numBlocks; i++) {
-        line += name + "-" + i;
-        if (i < numBlocks) {
-          line += ",";
+        int name = 1;
+        for(String block : characteristics) {
+            String line = "";
+            line += "<p> Characteristic " + name + ": [ ";
+            int numBlocks = Integer.parseInt(block);
+
+            for(int i = 1; i <= numBlocks; i++) {
+                line += name + "-" + i;
+                if (i < numBlocks) {
+                    line += ",";
+                }
+                line += " ";
+            }
+            line += "]</p>";
+            name += 1;
+            out.println(line);
         }
-        line += " ";
-      }
-      line += "]</p>";
-      name += 1;
-      out.println(line);
+
+        out.println("  </div>");
+        out.println("</div>");
+
+        out.println("</body>");
+
+        printTail(out);
+        out.close();
+
     }
 
-    out.println("  </div>");
-    out.println("</div>");
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html"); // Tells the web container what we're sending back
+        PrintWriter out = response.getWriter(); // Make it appear as if we're "writing" to the browser window
+        printHeader(out);
+        printBody(out);
+        printTail(out);
+        out.close();
+    } // end doGet()
 
-    out.println("</body>");
+    private void printHeader(PrintWriter out) {
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>SWE 432-001 Assignment 4</title>");
+        out.println("<meta name='description' content='SWE 432-001 Assignment 4'>");
+        out.println("<meta name='author' content='Charlie Duong, Khai Nguyen, Jesse Tran'>");
+        out.println("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
+        // out.println("<link rel='icon' href='../assets/icon.png'>");
 
-    printTail(out);
-    out.close();
-  }
+        // CSS FILES MUST BE WITHIN "WEBAPP" FOLDER
+        out.println("<link rel='stylesheet' type='text/css' href='/styles/assignment3.css'/>");
+        out.println("<link rel='stylesheet' type='text/css' href='/styles/default.css'/>");
 
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    response.setContentType("text/html"); // Tells the web container what we're sending back
-    PrintWriter out = response.getWriter(); // Make it appear as if we're "writing" to the browser window
-    printHeader(out);
-    printBody(out);
-    printTail(out);
-    out.close();
-  } // end doGet()
+        out.println("<script src='js/assignment4.js'></script>");
 
-  private void printHeader(PrintWriter out) {
-    out.println("<html>");
-    out.println("<head>");
-    out.println("<title>SWE 432-001 Assignment 4</title>");
-    out.println("<meta name='description' content='SWE 432-001 Assignment 4'>");
-    out.println("<meta name='author' content='Charlie Duong, Khai Nguyen, Jesse Tran'>");
-    out.println("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
-    // out.println("<link rel='icon' href='../assets/icon.png'>");
+        out.println("</head>");
+    }
 
-    // CSS FILES MUST BE WITHIN "WEBAPP" FOLDER
-    out.println("<link rel='stylesheet' type='text/css' href='/styles/assignment3.css'/>");
-    out.println("<link rel='stylesheet' type='text/css' href='/styles/default.css'/>");
+    private void printBody(PrintWriter out) {
+        out.println("<body>");
 
-    out.println("<script src='js/Assignment4.js'></script>");
+        out.println("<div>");
+        out.println("  <div style='text-align: center;'>");
+        out.println("    <h1 style='margin-bottom: 0px; padding-top: 5%;'>SWE 432-001 ASSIGNMENT #4</h1>");
+        out.println("    <h2>Charlie Duong &nbsp; | &nbsp; Khai Nguyen &nbsp; | &nbsp; Jesse Tran</h2>");
+        out.println("  </div>");
 
-    out.println("</head>");
-  }
+        out.println("  <div style='background-color: white; border-radius: 5px; width: 50%; margin: auto;'>");
+        out.println("    <form method='post' action='assignment4'>");
+        out.println("      <table id='form' style='text-align: right; margin: auto; margin-top: 5%; width: 100% !important;'>");
+        out.println("        <tr>");
+        out.println("          <td>Enter a positive number of characteristics: </td>");
+        out.println("          <td>");
+        out.println("            <input style='width: 100%;' type='number' min='1' id='totalInputs' placeholder='1' autofocus required>");
+        out.println("          </td>");
+        out.println("          <td>");
+        out.println("            <input style='width: 100%;' id='gen' type='button' class='primary' value='Generate' style='width: 100%;' onclick='generateButton()'>");
+        out.println("          </td>");
+        out.println("        </tr>");
 
-  private void printBody(PrintWriter out) {
-    out.println("<body>");
+        // GENERATED TABLE SHOULD GO HERE
 
-    out.println("<div>");
-    out.println("   <div style='text-align: center;'>");
-    out.println("       <h1 style='margin-bottom: 0px; padding-top: 5%;'>SWE 432-001 ASSIGNMENT #4</h1>");
-    out.println("       <h2>Charlie Duong &nbsp; | &nbsp; Khai Nguyen &nbsp; | &nbsp; Jesse Tran</h2>");
-    out.println("</div>");
+        out.println("        <tr>");
+        out.println("          <td></td>");
+        out.println("          <td></td>");
+        out.println("          <td style='text-align: right;'>");
+        out.println("            <input type='reset' class='secondary' value='Reset' style='width: fit-content; width: 100%;' onclick='resetButton();'>");
+        out.println("            <input type='submit' class='primary' value='Submit' style='width: fit-content; width: 100%;' onclick='checkInputs();'>");
+        out.println("          </td>");
+        out.println("        </tr>");
+        out.println("      </table>");
+        out.println("    </form>");
+        out.println("  </div>");
 
-    out.println("<div style='background-color: white; border-radius: 5px; width: 50%; margin: auto;'>");
-    out.println("<form method='post' action='https://cs.gmu.edu:8443/offutt/servlet/formHandler'>");
-    out.println("<table id='form' style='text-align: right; margin: auto; margin-top: 5%; width: 100% !important;'>");
-    out.println("<tr>");
-    out.println("<td>Enter a positive number of characteristics:</td>");
-    out.println("<td>");
-    out.println(
-        "<input style='width: 100%;' type='number' min='1' id='totalInputs' placeholder='1' autofocus required>");
-    out.println("</td>");
-    out.println("<td>");
-    out.println(
-        "<input style='width: 100%;' id='gen' type='button' class='primary' value='Generate' style='width: 100%;' onclick='generateButton()'>");
-    out.println("</td>");
-    out.println("</tr>");
+        out.println("  <br></br>");
+        out.println("  <hr>");
+        out.println("  </hr>");
+        out.println("  <br></br>");
 
-    // GENERATED TABLE SHOULD GO HERE
+        // COLLABORATION SUMMARY  
+        out.println("  <div style='background-color: white; border-radius: 5px; width: 50%; margin: auto;'>");
+        out.println("    <div style='padding: 10px'>");
+        out.println("      <h3>Collaboration Summary:</h3>");
+        out.println("        <p>Khai Nguyen: Converted assignment 3 frontend to java servlet and relinked necessary files i.e. css and js.</p>");
+        out.println("        <p>Jesse Tran: Contributed towards reorganized doGet method and implementing getpost. redid form data.</p>");
+        out.println("        <p>Charlie Duong: Contributed towards implementation of getPost, output formatting, and integrated javascript file into doGet.</p>");
+        out.println("    </div>");
+        out.println("  </div>");
+        out.println("</div>");
 
-    out.println("<tr>");
-    out.println("<td></td>");
-    out.println("<td></td>");
-    out.println("<td style='text-align: right;'>");
-    out.println(
-        "<input type='reset' class='secondary' value='Reset' style='width: fit-content; width: 100%;' name='reset'>");
-    out.println(
-        "<input type='submit' class='primary' value='Submit' style='width: fit-content; width: 100%;' onclick='checkInputs();'>");
-    out.println("</td>");
-    out.println("</tr>");
-    out.println("</table>");
-    out.println("</form>");
-    out.println("</div>");
+        out.println("</body>");
+    }   
 
-    out.println("<br></br>");
-    out.println("<hr>");
-    out.println("</hr>");
-    out.println("<br></br>");
-
-    // COLLABORATION SUMMARY
-    out.println("<div style='background-color: white; border-radius: 5px; width: 50%; margin: auto;'>");
-    out.println("<div style='padding: 10px'>");
-    out.println("<h3>Collaboration Summary:</h3>");
-    out.println("        <p>Khai Nguyen: Converted assignment 3 frontend to java servlet and relinked necessary files i.e. css and js.</p>");
-    out.println("        <p>Jesse Tran: Contributed towards reorganized doGet method and implementing getpost. redid form data.</p>");
-    out.println("        <p>Charlie Duong: Contributed towards implementation of getPost, output formatting, and integrated javascript file into doGet.</p>");
-    out.println("</div>");
-    out.println("</div>");
-    out.println("</div>");
-    out.println("</body>");
-  }
-
-  private void printTail(PrintWriter out) {
-    out.println("");
-    out.println("</html>");
-  } // End PrintTail
-
+    private void printTail(PrintWriter out) {
+        out.println("");
+        out.println("</html>");
+    }
 }
